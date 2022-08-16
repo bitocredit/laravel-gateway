a5f01c557a330e372f0597b45993eef898f39adf
reza motahary <56221733+rezaMotahary@users.noreply.github.com>
2022-08-16 05:09:52 -0700

    updated Api class

--- src/Api.php
+++ src/Api.php
@@ -9,8 +9,7 @@ class Api extends ApiPhp
 
     public function __construct()
     {
-        parent::__construct(config("gateway.token"));
-        $this->config['baseURL'] = config("gateway.base_url");
+        parent::__construct(config("gateway.base_url") , config("gateway.token"));
     }
 
 }
